85b80716910dcf2fd6e711cb251ab4568e9e47ac
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-09-01 11:53:50 +0100

    added main code, test 5 & 6 failing

--- orderOfJobs.js
+++ orderOfJobs.js
@@ -29,18 +29,31 @@ const orderOfJobs = jobs => {
   for (let keyJob in jobsObject) {
     // loop over the data
     if(jobsObject[keyJob] === keyJob) return ("Jobs cant depend on itself");
-    //returns an error if a job is dependant on itself othewise
+    //returns an error if a job is dependant on itself othewise continue to loop
     if(jobsObject[keyJob].length === 0) {
       if(!correctOrderOfJobs.includes(keyJob)) correctOrderOfJobs.push(keyJob)
       //push jobs without any dependancies into the final result
     }
-    
-    // else {
+   
+    else {
+      if(correctOrderOfJobs.includes(jobsObject[keyJob]) 
+        && correctOrderOfJobs.includes(keyJob)) { 
+          correctOrderOfJobs.push(jobsObject[keyObject], keyObject)
+      }
+      //if job or dependency job are not in the array add in the correct order
 
+      else if (correctOrderOfJobs.includes(keyJob) && !correctOrderOfJobs.includes(jobsObj[keyJob])) {
+        let keyJobIndex = correctOrderOfJobs.indexOf(keyJob);
+        correctOrderOfJobs.splice(keyJobIndex, 0, jobsObject[keyJob]);
+      }
+      //if key job is already in the array add the dependency before it
 
-    // }
-
-
+      else {
+        let keyJobIndex = correctOrderOfJobs.indexOf(jobsObject[keyJob]);
+        correctOrderOfJobs.splice(keyJobIndex + 1, 0, keyJob);
+        //but if the depenency is in the array insert key after it
+      }
+    }
   }
 
   return correctOrderOfJobs;
